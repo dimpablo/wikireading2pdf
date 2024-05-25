59c500271c6d855b9e9619477337562b609dc825
dimpablo <119797859+dimpablo@users.noreply.github.com>
2024-05-25 16:04:21 +0300

    Update ops.sh

--- ops.sh
+++ ops.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
-# Заходим в папку htms
+
 cd htms
 
-# Считаем все *.htm файлы в папке в переменную $num
+# Count all *.htm and save in $num
 num=$(ls -1 *.htm | wc -l)
 
-# Создаем for-loop длиной от 1 до $num-1
 for ((i=1; i<$num+1; i++))
 do
-  # Внутри него берем файл с названием $num и расширением htm
+  # take a file
   file="${i}.htm"
 
-  # Производим с ним следующую процедуру: sed '/<amp-ad/,/<\/amp-ad>/d' 1.htm > 1.htm (вместо 1.htm имя текущего файла)
+  # remove Ads
   sed '/<amp-ad/,/<\/amp-ad>/d' "$file" > "$file.tmp"
   mv "$file.tmp" "$file"
 
-  # Затем sed -n '/<article class=/,/<\/article>/p' 1.htm > 1.htm (вместо 1.htm имя текущего файла)
+  # Remove unnecessary junk
   sed -n '/<article class=/,/<\/article>/p' "$file" > "$file.tmp"
   mv "$file.tmp" "$file"
 
-  # Затем sed 's/amp-img/img/g' 1.htm > 1.htm (вместо 1.htm имя текущего файла)
+  # Correct images
   sed 's/amp-img/img/g' "$file" > "$file.tmp"
   mv "$file.tmp" "$file"
 done
